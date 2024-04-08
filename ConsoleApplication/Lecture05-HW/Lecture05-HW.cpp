1210202eaa01b1650b2e9e6851a7a6b0f9bcaba2
배현준 <9247bhj@gmail.com>
2024-04-08 16:37:40 +0900

    5주차 과제

--- ConsoleApplication/Lecture05-HW/Lecture05-HW.cpp
+++ ConsoleApplication/Lecture05-HW/Lecture05-HW.cpp
@@ -8,8 +8,64 @@
 //3. 우클릭하고 우측으로 드래그한만큼 확대 좌측으로 드래그한만큼 축소를 해주세요.
 //   (별의 중점을 기준으로 확대/축소가 돼야합니다.)
 
-float moveFactor = 0.0f;
-float scaleFactor = 1.0f;
+float scale = 1.0f;
+float xOffset = 0.0f;
+float yOffset = 0.0f;
+bool isMouseDown = false;
+float dragStartX = 0.0f;
+float dragStartY = 0.0f;
+
+void onMouseButton(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT) {
+		if (action == GLFW_PRESS) {
+			isMouseDown = true;
+			//좌클릭 드래그 시작 위치 저장
+			double xpos, ypos;
+			glfwGetCursorPos(window, &xpos, &ypos);
+			dragStartX = static_cast<float>(xpos);
+			dragStartY = static_cast<float>(ypos);
+		}
+		else if (action == GLFW_RELEASE) {
+			isMouseDown = false;
+		}
+		else if (button == GLFW_MOUSE_BUTTON_RIGHT) {
+			if (action == GLFW_PRESS) {
+				isMouseDown = true;
+				//우클릭 드래그 시작 위치 저장
+				double xpos, ypos;
+				glfwGetCursorPos(window, &xpos, &ypos);
+				dragStartX = static_cast<float>(xpos);
+				dragStartY = static_cast<float>(ypos);
+			}
+			else if (action == GLFW_RELEASE) {
+				isMouseDown = false;
+			}
+		}
+	}
+}
+
+
+void onMouseMove(GLFWwindow* window, double xpos, double ypos) {
+	if (isMouseDown) {
+		//마우스 좌클릭 드래그 중일 때 삼각형 위치 업데이트
+		float deltaX = xpos - dragStartX;
+		float deltaY = ypos - dragStartY;
+		xOffset += deltaX / 500;
+		yOffset -= deltaY / 500;
+		//드래그 시작 위치 재설정
+		dragStartX = xpos;
+		dragStartY = ypos;
+	}
+	else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+		//마우스 우클릭 드래그 중일 때 삼각형 확대/축소
+		float deltaX = xpos - dragStartX;
+		//삼각형 중점을 기준으로 확대/축소
+		float scaleFactor = 1.0f + (deltaX / 1000.0f);
+		scale *= scaleFactor;
+		//드래그 시작 위치 재설정
+		dragStartX = xpos;
+	}
+}
 
 void errorCallback(int error, const char* description)
 {
@@ -22,41 +78,49 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 	{
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 	}
-	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
-	{
-		moveFactor += 0.01f;
-	}
-	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
-	{
-		scaleFactor += 1.0f;
-	}
 }
 
 int render()
 {
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(xOffset, yOffset, 0.0f);
+	glScalef(scale, scale, 1.0f);
+
 	glBegin(GL_TRIANGLES);
 	//점1
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex2f(0.0f * scaleFactor, (1.0f + moveFactor) * scaleFactor);
+	glVertex2f(0.0f, 1.0f);
 	//점2
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex2f(-0.4f * scaleFactor, (-0.2f + moveFactor) * scaleFactor);
+	glVertex2f(0.62f, -1.0f);
 	//점3
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex2f(0.6f * scaleFactor, (-1.0f + moveFactor) * scaleFactor);
+	glVertex2f(-0.4f, -0.24f);
 	glEnd();
 
 	glBegin(GL_TRIANGLES);
 	//점1
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex2f(1.0f * scaleFactor, (0.2f + moveFactor) * scaleFactor);
-	glEnd();
+	glVertex2f(0.0f, 1.0f);
 	//점2
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex2f(1.0f * scaleFactor, (1.0f + moveFactor) * scaleFactor);
+	glVertex2f(-0.62f, -1.0f);
 	//점3
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex2f(0.5f * scaleFactor, (-0.5f + moveFactor) * scaleFactor);
+	glVertex2f(0.4f, -0.24f);
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	//점1
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-1.0f, 0.23f);
+	//점2
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.0f, -0.55f);
+	//점3
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(1.0f, 0.23f);
 	glEnd();
 
 	return 0;
@@ -80,6 +144,8 @@ int main(void)
 	glfwMakeContextCurrent(window);
 	glfwSetErrorCallback(errorCallback);
 	glfwSetKeyCallback(window, keyCallback);
+	glfwSetMouseButtonCallback(window, onMouseButton);
+	glfwSetCursorPosCallback(window, onMouseMove);
 
 	while (!glfwWindowShouldClose(window))
 	{
