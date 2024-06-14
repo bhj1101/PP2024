5cac7f00b126a12c70324d21f17c1d9c3112210f
bhj1101 <9247bhj@gmail.com>
2024-06-14 15:44:33 +0900

    Exam

---
+++ ConsoleApplication/FinalExam/Object.h
@@ -0,0 +1,83 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <iostream>
+// Base class
+class Object {
+public:
+    virtual void OnCollisionEnter(Object& other) {
+    }
+};
+
+// Derived classes
+class Player : public Object {
+public:
+    void OnCollisionEnter(Object& other) override {
+    }
+    void Render() {
+        // 빨간색 정사각형 (50cm x 50cm) 및 3cm 두께 테두리 렌더링
+        glBegin(GL_QUADS);
+        glColor3f(1.0f, 0.0f, 0.0f); // 빨강
+        glVertex2f(-0.25f, -0.25f); // 왼쪽 아래
+        glVertex2f(0.25f, -0.25f); // 오른쪽 아래
+        glVertex2f(0.25f, 0.25f); // 오른쪽 위
+        glVertex2f(-0.25f, 0.25f); // 왼쪽 위
+        glEnd();
+
+        // 테두리 렌더링 (검정색, 3cm 두께)
+        glBegin(GL_LINE_LOOP);
+        glLineWidth(3.0f);
+        glColor3f(0.0f, 0.0f, 0.0f); // 검정색
+        glVertex2f(-0.28f, -0.28f);
+        glVertex2f(0.28f, -0.28f);
+        glVertex2f(0.28f, 0.28f);
+        glVertex2f(-0.28f, 0.28f);
+        glEnd();
+    }
+};
+
+class EnemyBlock : public Object {
+public:
+    void OnCollisionEnter(Object& other) override {
+    }
+    void Render(float x, float y, float height) {
+        // 주어진 위치 (x, y) 및 높이(height)로 적 블록 렌더링 (파란색, 50cm x height)
+        glBegin(GL_QUADS);
+        glColor3f(0.0f, 0.0f, 1.0f); // 파란색
+        glVertex2f(x - 25.0f, y);
+        glVertex2f(x + 25.0f, y);
+        glVertex2f(x + 25.0f, y + height);
+        glVertex2f(x - 25.0f, y + height);
+        glEnd();
+    }
+};
+
+class Floor : public Object {
+public:
+    void OnCollisionEnter(Object& other) override {
+    }
+    void Render() {
+        // 노란색 바닥 렌더링 (800cm x 100cm)
+        glBegin(GL_QUADS);
+        glColor3f(1.0f, 0.8f, 0.6f); // 노란색
+        glVertex2f(-400.0f, -100.0f);
+        glVertex2f(400.0f, -100.0f);
+        glVertex2f(400.0f, 0.0f);
+        glVertex2f(-400.0f, 0.0f);
+        glEnd();
+    }
+};
+
+class Star : public Object {
+public:
+    void OnCollisionEnter(Object& other) override {
+    }
+    void Render() {
+        
+    }
+};
+
+
+int PhysicsAABB(Object A, Object B)
+{
+    return 0;
+}
---
+++ ConsoleApplication/FinalExam/main.cpp
@@ -0,0 +1,102 @@
+﻿#pragma comment(lib, "Opengl32.lib")
+
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#else
+#define DBG_NEW new
+#endif
+
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <chrono>
+#include <thread>
+#include <cmath>
+#include "Object.h"
+
+Player player;
+EnemyBlock enemyBlocks[2] = { EnemyBlock(), EnemyBlock() };
+//Floor floor; 
+Star star;
+
+float enemyBlockXPositions[2] = { -200.0f, 200.0f }; // 적 블록 X 위치
+float enemyBlockHeights[2] = { 100.0f, 300.0f }; // 적 블록 높이
+
+void errorCallback(int error, const char* description)
+{
+	printf("GLFW Error: %s", description);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+}
+
+int Physics()
+{
+	return 0;
+}
+
+int Initialize()
+{
+	return 0;
+}
+
+int Update()
+{
+	return 0;
+}
+
+
+int Render()
+{
+	player.Render();
+	enemyBlocks[0].Render(enemyBlockXPositions[0], -100.0f, enemyBlockHeights[0]);
+	enemyBlocks[1].Render(enemyBlockXPositions[1], -100.0f, enemyBlockHeights[1]);
+	//floor.Render();
+	star.Render();
+	return 0;
+}
+
+int main(void)
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+	//glfw라이브러리 초기화
+	if (!glfwInit())
+		return -1;
+
+	
+	GLFWwindow* window;
+	window = glfwCreateWindow(800, 600, "Google Dino Run Copy Game", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetErrorCallback(errorCallback);
+	glfwSetKeyCallback(window, keyCallback);
+
+	Initialize();
+
+	glClearColor(0.0f, 30/255.0f, 100/255.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+		Physics();
+		Update();
+		Render();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+
+}
